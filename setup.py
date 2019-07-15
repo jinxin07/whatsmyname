2fefcc53d7ee427885f2bc36c43ec4a25292fe69
Xin <jin.xin@accenture.com>
2019-07-15 15:03:21 +0100

    name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="whats_my_name_jx",
+    name="whatsmynamejx",
     version="1.0.0",
     author="Jin Xin",
     author_email="jin.xin@accenture.com",
