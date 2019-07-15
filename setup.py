d8c00d74180443e76b6c6349a2d0e3188779e8f8
Xin <jin.xin@accenture.com>
2019-07-15 12:55:44 +0100

    setup stuff

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-from setuptools import setup
+import setuptools
 
-with open ("README.txt", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup (
-    name = 'Whats_My_name',
-    version = '1.0.0',
-    description = 'Say my name',
-    py_modules = ["script"],
-    package_dir = {'new_repo': 'main'},
-
+setuptools.setup(
+    name="whats_my_name_jx",
+    version="1.0.0",
+    author="Jin Xin",
+    author_email="jin.xin@accenture.com",
+    description="Say my name",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jinxin07/whatsmyname",
+    packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: MIT"
-    ]
-    long_description = long_description,
-    long_description_content_type = "text/markdown"
-    
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["main", "test"],
+    include_package_data=True,
 )
